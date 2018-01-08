28dc43462a34a95b5af893e4be5e8491fc894ac6
Mark Jacobs <jacobs5977@gmail.com>
2018-01-07 22:14:21 -0600

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
+const gulpif = require('gulp-if');
+const imagemin = require('gulp-imagemin');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const uncss = require('gulp-uncss');
+const useref = require('gulp-useref');
+const browserSync = require('browser-sync').create();
+
+const onError = function(err) {
+  console.log(err.message);
+  this.emit('end');
+};
+
+gulp.task('css', function() {
+  return gulp.src('src/sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', onError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('copy', function() {
+  return gulp.src('src/**/*.html')
+    .pipe(useref())
+    .pipe(gulpif('*.js', sourcemaps.init()))
+    .pipe(gulpif('*.js', uglify()))
+    .pipe(gulpif('*.js', sourcemaps.write('.')))
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'dist'
+    }
+  })
+});
+
+gulp.task('watch', ['browserSync', 'css'], function() {
+  gulp.watch('src/sass/**/*.scss', ['css']);
+  gulp.watch('src/**/*.+(html|js)', ['copy']);
+});
---
+++ src/sass/README.md
@@ -0,0 +1,2 @@
+### Network Next Gen
+This repository contains the static site assets for future use in Network Next Gen's custom Wordpress theme.
