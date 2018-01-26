383022b187b33253333076f4d5f5fbbe68b0c3a2
Mark Jacobs <jacobs5977@gmail.com>
2018-01-26 09:39:35 -0600

    Clean up grid builder method

--- src/js/app.js
+++ src/js/app.js
@@ -8,37 +8,39 @@ nextGen.ready = function(callback) {
   }
 };
 
-nextGen.updateRows = function() {
-  var bodyStyle = window.getComputedStyle(document.querySelector('body'));
-  var numOfRows = parseInt(bodyStyle.getPropertyValue('--numOfRows'));
-  var numOfItems = parseInt(bodyStyle.getPropertyValue('--numOfItems'));
-  var totalItems = document.querySelectorAll('.city-picker__item').length;
-  var totalRowsForItems = Math.floor(totalItems / 2.5);
-  if (numOfItems !== totalItems) {
-    document.documentElement.style.setProperty('--numOfItems', numOfItems);
-  }
-  if (numOfRows !== totalRowsForItems) {
-    document.documentElement.style.setProperty('--numOfRows', totalRowsForItems * 2);
-  }
-};
-
 nextGen.buildGrid = function() {
   var items = Array.from(document.querySelectorAll('.city-picker__item'));
-  var n = Math.floor(items.length / 2.5);
-  var counter;
+  var totalRows = Math.floor(items.length / 2.5);
+  var i;
 
-  for (counter = 0; counter <= n; counter++) {
-    if (counter && counter % 2 === 0) {
-      var duo = items.slice(5 * counter + 3, 5 * counter + 5);
-      duo.forEach(function(node, i) {
-        node.style.gridRow = counter + 2 + ' / span 2';
-      });
+  for (i = 0; i <= totalRows; i++) {
+    var shiftOddRowsBy;
+    var  shiftEvenRowsBy;
+
+    if (i === 0) {
+      shiftOddRowsBy = 1;
+      shiftEvenRowsBy = 2;
+    } else if (i % 2 === 1) {
+      shiftOddRowsBy = i + 2;
+      shiftEvenRowsBy = i + 3;
     } else {
-      var trio = items.slice(5 * counter, 5 * counter + 3);
-     trio.forEach(function(node, i) {
-        node.style.gridRow = counter + ' / span 2';
-      });
+      shiftOddRowsBy = i + 3;
+      shiftEvenRowsBy = i + 4;
     }
+
+    // Arrange first three of every five into odd rows
+    items
+      .slice(5 * i, 5 * i + 3)
+      .forEach(function(item) {
+        item.style.gridRow = shiftOddRowsBy + ' / span 2';
+      });
+
+    // Arrange last two of five into even rows
+    items
+      .slice(5 * i + 3, 5 * i + 5)
+      .forEach(function(item) {
+        item.style.gridRow = shiftEvenRowsBy + ' / span 2';
+      });
   }
 };
 
